1db284dada30354602ca6e15440e0c0adab16862
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2025-01-20 12:09:45 +0530

    ALTERATIONS:
    added alternative dependency
    index file corrections
    handling music controls permissions

--- index.js
+++ index.js
@@ -7,4 +7,3 @@ import { name as appName } from './app.json';
 import { playbackService } from './musicPlayerService';
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => playbackService);
